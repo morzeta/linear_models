61a5c34c5747a8bac9a56814a129a412ec67bf66
Morteza Eftekhar <morz.eftekhar@gmail.com>
2024-11-27 13:37:29 +0100

    feature profile
    add data profile for better inspection and intuitions

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # linear_models
 LSTAT2120 - Linear Models
+
+The Data in use:
+https://archive.ics.uci.edu/dataset/360/air+quality
+
+Install requirements in with:
+pip install -r /path/to/requirements.txt
+
+Run:
+run test.py to produce the data profile
--- test.py
+++ test.py
@@ -1,8 +1,24 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from ydata_profiling import ProfileReport
 from preparation import read_data
 from utils import set_random_seed
 
+# set seed for reproducible results
 set_random_seed(1)
+
+# read and prepare data
 x, y = read_data()
 
-print(x.describe())
-print(y.head())
+# data normalisation
+# x = (x-x.mean())/x.std()
+
+# correlation matrix
+# sns.heatmap(x.corr(), annot=True, linewidths=.4)
+# plt.title('Heatmap of correlation between variables', fontsize=16)
+# plt.show()
+
+# data profile
+profile = ProfileReport(x, title="Profiling Report", explorative=True)
+profile.to_file("report.html")
